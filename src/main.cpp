296689ad964ea831511e94f900617657af009224
maguro256 <1030seima1030@gmail.com>
2026-02-13 02:45:07 +0900

    add renderer texture

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ const int WINDOW_HEIGHT = 360;
 
 int main(int argc, char* argv[]) {
     
+    //初期化
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
         return -1;
@@ -13,22 +14,68 @@ int main(int argc, char* argv[]) {
 
     std::cout << "SDL Initialized successfully!" << std::endl;
 
+    //window作成
     SDL_Window* window = SDL_CreateWindow(
-        "My Engine - Low Level",
+        "My Engine",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        800, 600,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
         SDL_WINDOW_SHOWN
     );
 
-    if (!window) {
-        std::cout << "Failed to create window!" << std::endl;
-        return -1;
+    if (!window) return -1;
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    
+    SDL_Texture* texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        WINDOW_WIDTH, WINDOW_HEIGHT
+    );
+
+    Uint32* buffer = new Uint32[WINDOW_WIDTH * WINDOW_HEIGHT];
+
+    bool isRunning = true;
+    SDL_Event event;
+
+    while (isRunning) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                isRunning = false; // ループを抜けるフラグ
+            }
+        }
+
+        // --- 更新処理---   
+        for (int y = 0; y < WINDOW_HEIGHT; ++y) {
+            for (int x = 0; x < WINDOW_WIDTH; ++x) {
+                
+                // 2次元座標 (x, y) を 1次元のインデックスに変換
+                int index = y * WINDOW_WIDTH + x;
+                
+                if(x < WINDOW_WIDTH/2){
+                    buffer[index] = 0xFFFF0000; 
+                } else {
+                    buffer[index] = 0xFF00FF00; 
+                }
+            }
+        }
+        
+        SDL_UpdateTexture(texture, NULL, buffer, WINDOW_WIDTH * sizeof(Uint32));
+
+        SDL_RenderClear(renderer);
+
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+
+        SDL_RenderPresent(renderer);
     }
+    
 
-    // 3秒待ってから閉じる
-    SDL_Delay(3000);
+    delete[] buffer;
 
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+
     return 0;
 }
